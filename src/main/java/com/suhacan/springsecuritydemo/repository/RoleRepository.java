832d2af6f1ce65a8aa8f66af3c201156dff52040
Suha Can Uluer <sculuer@gmail.com>
2023-05-10 17:34:39 +0300

    JpaRepository changed of CrudRepository

--- src/main/java/com/suhacan/springsecuritydemo/repository/RoleRepository.java
+++ src/main/java/com/suhacan/springsecuritydemo/repository/RoleRepository.java
@@ -2,8 +2,9 @@ package com.suhacan.springsecuritydemo.repository;
 
 import com.suhacan.springsecuritydemo.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends CrudRepository<Role, Long> {
 }
